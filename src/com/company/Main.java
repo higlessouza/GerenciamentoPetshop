92834e88a04c6c28278316e8ba5678159ec5147f
Higles Souza <43656269+higlessouza@users.noreply.github.com>
2020-10-03 17:16:51 -0300

    Alteracao Main
    * Inciando aplicação
    * Retorno para o usuário de qual o melhor pet Shop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.text.NumberFormat;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        GerenciamentoPetshop gerenciamentoPetshop = new GerenciamentoPetshop(new Date("03/08/2018"), 3, 5);
+        Petshop petshop = gerenciamentoPetshop.ProcurarPetshop();
+        System.out.println("O canil mais adequado é o " + petshop.getNome() + " com o preço de " + petshop.getOrcamentoFormatado());
     }
 }
